76dea8c5c1e1465cb805036d230d2002a937ea6f
Anton Serra <aserratorta@gmail.com>
2016-04-25 22:21:23 +0200

    add Artist entity

---
+++ src/AppBundle/Entity/Artist.php
@@ -0,0 +1,217 @@
+<?php
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Traits\DescriptionTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class Artist
+ *
+ * @category Entity
+ * @package  AppBundle\Entity
+ * @author   Anton Serra <aserratorta@gmail.com>
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArtistRepository")
+ * @Vich\Uploadable
+ */
+class Artist extends AbstractBase
+{
+    use DescriptionTrait;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var File
+     *
+     * @Vich\UploadableField(mapping="coworker", fileNameProperty="imageName")
+     * @Assert\File(
+     *     maxSize = "10M",
+     *     mimeTypes = {"image/jpg", "image/jpeg", "image/png", "image/gif"}
+     * )
+     * @Assert\Image(minWidth = 1200)
+     */
+    private $imageFile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Url(checkDNS=true)
+     */
+    private $urlVimeo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Url(checkDNS=true)
+     */
+    private $urlSoundCloud;
+
+    /**
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="Artist", mappedBy="event")
+     */
+    private $events;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * Artist constructor.
+     */
+    public function __construct($events)
+    {
+        $this->events = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Artist
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Set imageFile
+     *
+     * @param File|UploadedFile $imageFile
+     *
+     * @return File
+     */
+    public function setImageFile(File $imageFile = null)
+    {
+        $this->imageFile = $imageFile;
+        if ($imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get imageFile
+     *
+     * @return File|UploadedFile
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * @param string $imageName
+     *
+     * @return Artist
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrlVimeo()
+    {
+        return $this->urlVimeo;
+    }
+
+    /**
+     * @param string $urlVimeo
+     *
+     * @return Artist
+     */
+    public function setUrlVimeo($urlVimeo)
+    {
+        $this->urlVimeo = $urlVimeo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrlSoundCloud()
+    {
+        return $this->urlSoundCloud;
+    }
+
+    /**
+     * @param string $urlSoundCloud
+     *
+     * @return Artist
+     */
+    public function setUrlSoundCloud($urlSoundCloud)
+    {
+        $this->urlSoundCloud = $urlSoundCloud;
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getEvents()
+    {
+        return $this->events;
+    }
+
+    /**
+     * @param ArrayCollection $events
+     *
+     * @return $this
+     */
+    public function setEvents(ArrayCollection $events)
+    {
+        $this->events = $events;
+        return $this;
+    }
+
+    public function __toString() {
+
+        return $this->getName() ? $this->getName() : '---';
+    }
+}
