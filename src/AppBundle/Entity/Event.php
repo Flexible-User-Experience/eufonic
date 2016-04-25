0fc28ae60f5e3399b3099e8916b83ff1a7a34d14
Anton Serra <aserratorta@gmail.com>
2016-04-25 22:48:27 +0200

    add Event entity

---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,159 @@
+<?php
+namespace AppBundle\Entity;
+
+use AppBundle\Entity\Traits\DateTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class Event
+ *
+ * @category Entity
+ * @package  AppBundle\Entity
+ * @author   Anton Serra <aserratorta@gmail.com>
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
+ */
+class Event extends AbstractBase
+{
+    use DateTrait;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $place;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float", name="longitude")
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(type="float", name="latitude")
+     */
+    private $latitude;
+
+    /**
+     * @var Artist
+     *
+     * @ORM\ManyToOne(targetEntity="Artist", inversedBy="events")
+     */
+    private $artist;
+
+    /**
+     * @var Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="events")
+     */
+    private $category;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * @return string
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * @param string $place
+     *
+     * @return Event
+     */
+    public function setPlace($place)
+    {
+        $this->place = $place;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     *
+     * @return Event
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     *
+     * @return Event
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * @return Artist
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * @param Artist $artist
+     *
+     * @return Event
+     */
+    public function setArtist(Artist $artist)
+    {
+        $this->artist = $artist;
+        return $this;
+    }
+
+    /**
+     * @return Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param Category $category
+     *
+     * @return Event
+     */
+    public function setCategory(Category $category)
+    {
+        $this->category = $category;
+        return $this;
+    }
+}
