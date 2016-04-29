7efe08875342d52cf275770cec1e92b741aaebd9
Anton Serra <aserratorta@gmail.com>
2016-04-30 01:18:14 +0200

    JMS Event entity

--- src/AppBundle/Entity/Traits/DateTrait.php
+++ src/AppBundle/Entity/Traits/DateTrait.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  * Date trait
@@ -18,6 +19,7 @@ Trait DateTrait
      * @var \DateTime
      *
      * @ORM\Column(type="date")
+     * @JMS\Groups({"list", "detail"})
      */
     private $date;
 
